3c5dc8206b1dcd302ef0d811108dc7d546b50eea
Dennis Yao <omethyn@gmail.com>
2022-10-28 11:46:42 -0500

    test out analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import App from "next/app";
+import Script from 'next/script'
 import Head from "next/head";
 import "../styles/global.css";
+
 
 type PageProps = {};
 
@@ -20,9 +21,12 @@ const MyApp = ({
         <title>Subset of Theoretical Practice</title>
         <meta name="description" content={meta} />
         <link rel="icon" href="/favicon.ico" />
+
       </Head>
 
+      <Script src="https://cdn.counter.dev/script.js" data-id="594404c5-6814-4476-a5af-d35ddc284e98" data-utcoffset="-5" />
       <Component {...pageProps} />
+
     </>
   );
 };
