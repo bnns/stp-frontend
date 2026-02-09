df51c1268748e872c06bb472765e69b6f67b4ae1
Dennis Yao <omethyn@gmail.com>
2026-02-09 12:21:20 -0600

    primer simulation

--- next.config.js
+++ next.config.js
@@ -3,5 +3,13 @@ module.exports = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
-  }
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/primer-simulation',
+        destination: '/primer-simulation.html',
+      },
+    ];
+  },
 }
